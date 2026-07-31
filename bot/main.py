4eee9ce2393dc0253396c8de8936ff493f0d9374
BOT-crypto123 <d26768790@gmail.com>
2026-07-31 14:59:40 -0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,18 @@
 from job import job, bot
 import threading
 import time
+import os
+from flask import Flask
+
+# Servidor falso para Render
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Bot de botones prendido..."
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 def run_bot():
     print("Bot de botones prendido...")
@@ -15,5 +27,6 @@ def run_alerts():
         time.sleep(3600)
 
 if __name__ == "__main__":
+    threading.Thread(target=run_web, daemon=True).start()
     threading.Thread(target=run_alerts, daemon=True).start()
     run_bot()
